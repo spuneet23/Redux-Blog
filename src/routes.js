e309271bf124eeb4ff482c802ba6452b767a5cd9
Puneet Singh <spuneet23@gmail.com>
2017-03-06 09:15:54 +0530

    First Commit

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostShow from './components/post_show';
+
+export default (
+   <Route path="/" component={App} >
+     <IndexRoute component={PostsIndex} />
+     <Route path="posts/new" component={PostsNew} />
+     <Route path="posts/:id"  component={PostShow} />
+   </Route>
+);
